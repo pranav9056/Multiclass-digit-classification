c133711842df6a7a9522373f95d8be4768feafec
Pranav <pranavjain3194@gmail.com>
2018-05-27 21:19:27 -0400

    added feedforward propogation

--- ex3/predict.m
+++ ex3/predict.m
@@ -7,29 +7,19 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
+theta1 = Theta1';
+z1  = X*theta1;
+a1 = sigmoid(z1);
+a1 = [ones(m, 1) a1];
+theta2 = Theta2';
+z2 = a1 * theta2;
+a2 = sigmoid(z2);
+[val,p] = max(a2,[],2);
 
 end
